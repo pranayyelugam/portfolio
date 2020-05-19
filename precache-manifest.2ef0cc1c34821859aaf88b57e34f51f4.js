a353365a8b808ce8fc20f257ef0ef57977ced4b8
Pranay Yelugam <root@SHRUTHIYs-MacBook-Pro.local>
2020-05-19 19:21:47 +0530

    Updates

---
+++ precache-manifest.2ef0cc1c34821859aaf88b57e34f51f4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b770ad0caf562d4e1747aba83c1f45a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f8a4355c0458651475a5",
+    "url": "/portfolio/static/css/main.a8440ad8.chunk.css"
+  },
+  {
+    "revision": "d5cce6018617d7ef98ca",
+    "url": "/portfolio/static/js/2.0cf1c686.chunk.js"
+  },
+  {
+    "revision": "11da4d0289ce192632a3fc6590a86fde",
+    "url": "/portfolio/static/js/2.0cf1c686.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f8a4355c0458651475a5",
+    "url": "/portfolio/static/js/main.5be4ddba.chunk.js"
+  },
+  {
+    "revision": "6fef3673099b4dcdb973",
+    "url": "/portfolio/static/js/runtime-main.fed7eca6.js"
+  },
+  {
+    "revision": "c1c92d0c804d46cb62814e7389725082",
+    "url": "/portfolio/static/media/GreatVibes-Regular.c1c92d0c.ttf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ea8a267c5bdae1bf868459ec06d00312",
+    "url": "/portfolio/static/media/profile_dp1.ea8a267c.jpg"
+  }
+]);
