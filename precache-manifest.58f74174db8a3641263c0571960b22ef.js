7b01c8b8b505559a025999720afa44a29eb9af5e
Pranay Yelugam <root@SHRUTHIYs-MacBook-Pro.local>
2020-08-23 06:13:54 +0530

    Updates

---
+++ precache-manifest.58f74174db8a3641263c0571960b22ef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "232ad4a3ac83190e222f5d8b7ef59c5a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c8eeab98036fe1b53f98",
+    "url": "/portfolio/static/css/main.a8f13e90.chunk.css"
+  },
+  {
+    "revision": "35f3a19446e55dcb04dc",
+    "url": "/portfolio/static/js/2.51914935.chunk.js"
+  },
+  {
+    "revision": "11da4d0289ce192632a3fc6590a86fde",
+    "url": "/portfolio/static/js/2.51914935.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c8eeab98036fe1b53f98",
+    "url": "/portfolio/static/js/main.bb8e8f54.chunk.js"
+  },
+  {
+    "revision": "6fef3673099b4dcdb973",
+    "url": "/portfolio/static/js/runtime-main.fed7eca6.js"
+  },
+  {
+    "revision": "c1c92d0c804d46cb62814e7389725082",
+    "url": "/portfolio/static/media/GreatVibes-Regular.c1c92d0c.ttf"
+  },
+  {
+    "revision": "ea8a267c5bdae1bf868459ec06d00312",
+    "url": "/portfolio/static/media/profile_dp1.ea8a267c.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.81b8b8a8184d5ddc663d4e44b83ae8a5.js"
+  "/portfolio/precache-manifest.58f74174db8a3641263c0571960b22ef.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
