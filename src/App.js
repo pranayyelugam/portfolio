b654edf00138262e386c840552783db20fb858a3
Pranay Yelugam <shruthiy@SHRUTHIYs-MacBook-Pro.local>
2020-08-23 05:44:03 +0530

    removed router path

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ import ProjectsComponent from './Components/Projects';
 function App() {
   return (
     <div className="App">
-      <Router basename={process.env.PUBLIC_URL}>
+      <Router>
         <Header />
         <div className="dynamic-area">
           <div className="site-content">
