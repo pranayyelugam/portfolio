e4fc8d6953e53588936d33c4dd22f328cd76f181
Pranay Yelugam <ypranayraj@gmail.com>
2021-06-14 23:57:11 -0400

    Updates

---
+++ precache-manifest.622283a3be4d50ddb1476d5b3980a5f9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eec5e6247fede52ef20fb54be13f460",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c1c29459b8e79533dda6",
+    "url": "/portfolio/static/css/main.5720a697.chunk.css"
+  },
+  {
+    "revision": "13d5bf741c58a2313a6d",
+    "url": "/portfolio/static/js/2.475ea6c2.chunk.js"
+  },
+  {
+    "revision": "11da4d0289ce192632a3fc6590a86fde",
+    "url": "/portfolio/static/js/2.475ea6c2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c1c29459b8e79533dda6",
+    "url": "/portfolio/static/js/main.2b7dc00d.chunk.js"
+  },
+  {
+    "revision": "6fef3673099b4dcdb973",
+    "url": "/portfolio/static/js/runtime-main.fed7eca6.js"
+  },
+  {
+    "revision": "c1c92d0c804d46cb62814e7389725082",
+    "url": "/portfolio/static/media/GreatVibes-Regular.c1c92d0c.ttf"
+  },
+  {
+    "revision": "e08616dd663f9ee0765dc3e738acd6c3",
+    "url": "/portfolio/static/media/pxl.e08616dd.jpg"
+  },
+  {
+    "revision": "d4fb54c8497d42da4bad49c44ac36e81",
+    "url": "/portfolio/static/media/resume.d4fb54c8.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.7de24a8fba636ee0132f3ac4898499f2.js"
+  "/portfolio/precache-manifest.622283a3be4d50ddb1476d5b3980a5f9.js"
 );
 
 self.addEventListener('message', (event) => {
