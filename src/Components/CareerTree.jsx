b1f2a56984e706fc16f5bb2bfb0613afee810300
Pranay Yelugam <pranay.yelugam@gmail.com>
2026-01-03 03:32:52 -0800

    Add custom favicon and fix image aspect ratios

--- src/Components/CareerTree.jsx
+++ src/Components/CareerTree.jsx
@@ -83,7 +83,7 @@ const CareerNode = ({ item, isLast, showCountry, isReverse }) => {
                             alt={`${item.location} monument`}
                             className="monument-icon"
                             width="100"
-                            height="80"
+                            height="100"
                         />
                     </div>
                 )}
@@ -113,7 +113,7 @@ const CareerNode = ({ item, isLast, showCountry, isReverse }) => {
                                 alt={`${item.location} monument`}
                                 className="monument-icon-mobile"
                                 width="60"
-                                height="48"
+                                height="60"
                             />
                         </div>
                     )}
