bfee13fa7c49eee05502c1fa0a2baf6a5ff036fa
Pranay Yelugam <pranay.yelugam@gmail.com>
2026-01-03 13:30:23 -0800

    Fix footer alignment: Remove inline margins

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -14,15 +14,15 @@ export default class Footer extends Component {
     render() {
         return (
             <div className="content-bottom">
-                <div style={{ marginRight: '1rem'  }}>
+                <div>
                     <a href="https://github.com/pranayyelugam" aria-label="GitHub Profile"><AiFillGithub size="25px" /></a>
                 </div>
 
-                <div style={{ marginRight: '1rem' }}>
+                <div>
                     <a href="mailto:ypranayraj@gmail.com" aria-label="Email Me"><GrMail size="25px" /></a>
                 </div>
 
-                <div style={{ marginRight: '1rem' }}>
+                <div>
                     <a href="https://scholar.google.com/citations?user=i4JN7mwAAAAJ&hl=en" aria-label="Google Scholar Profile"><SiGooglescholar size="25px" /></a>
                 </div>
             </div>
