64570e87bdb7e9baa5006aceb6e29980779d7f3f
Pranay Yelugam <pranay.yelugam@gmail.com>
2026-01-03 14:39:32 -0800

    Feat: Update photography with Instagram highlights

--- src/Components/images.jsx
+++ src/Components/images.jsx
@@ -1,20 +1,31 @@
+import Insta1 from '../Images/insta_1.jpg';
+import Insta2 from '../Images/insta_2.jpg';
+import Insta3 from '../Images/insta_3.jpg';
+
 const Images = [
     {
         id: 1,
-        src: "https://images.pixieset.com/33685184/4ea80d464a0fe31507af2d4271203f93-large.jpg",
-        alt: "Acadia Sunrise",
-        title: "Sunrise from Cadillac Mountain",
-        description:"",
-        tags: ["Nature", "Sunrise", "Maine"]
+        src: Insta1,
+        alt: "Instagram Highlight 1",
+        title: "Photography Highlight 1",
+        description: "",
+        tags: ["Photography", "Highlight"]
     },
     {
         id: 2,
-        src: "https://images.pixieset.com/33685184/1f85f23cee2d97ab4a9e038aecc44afc-large.jpg",
-        alt: "Carousel @ Santa Cruz Boardwalk",
-        title: "Carousel @ Santa Cruz Boardwalk",
-        description:
-            "",
-        tags: ["Urban", "California"]
+        src: Insta2,
+        alt: "Instagram Highlight 2",
+        title: "Photography Highlight 2",
+        description: "",
+        tags: ["Photography", "Highlight"]
+    },
+    {
+        id: 3,
+        src: Insta3,
+        alt: "Instagram Highlight 3",
+        title: "Photography Highlight 3",
+        description: "",
+        tags: ["Photography", "Highlight"]
     }
 ]
 
