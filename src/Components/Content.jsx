a80634425ec217ccdf34b19791c07e3a8149cb37
Pranay Yelugam <pranay.yelugam@gmail.com>
2026-01-03 03:44:11 -0800

    Optimize LCP: Use fetchPriority='high' and eager loading for profile image

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -8,7 +8,7 @@ const LeftContainer = () => {
     return (
         <header className="content-left">
             <div className="dp">
-                <img src={img} alt="Pranay Yelugam" style={{ borderRadius: '5%' }} loading="lazy" width="288" height="384" />
+                <img src={img} alt="Pranay Yelugam" style={{ borderRadius: '5%' }} fetchPriority="high" width="288" height="384" />
             </div>
             <h1 className="stylish-name">Pranay Yelugam</h1>
             <h2 className="stylish-name job-title">Senior Software Engineer @ Verkada</h2>
