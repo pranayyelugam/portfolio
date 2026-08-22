1d861b974acd26494883495b26169bfd9f044eb8
pranayyelugam <ypranayraj@gmail.com>
2026-08-22 10:54:41 -0700

    Updates

---
+++ assets/Photography-BgBxfnXt.js
@@ -0,0 +1,1 @@
+import{j as a,I as r}from"./index-CNazuLMj.js";import"./vendor-Bebjssv0.js";import"./ui-D05ld0mp.js";const m=()=>a.jsx("div",{className:"photography-page",children:a.jsx("div",{className:"gallery-grid",children:r.map(s=>a.jsx("div",{className:"gallery-item",children:a.jsx("img",{src:s.src,alt:s.alt,className:"gallery-img",loading:"lazy"})},s.id))})});export{m as default};
